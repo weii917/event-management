520ccffb9205b36afac511047b84e44ddcb8b38d
wei <wendyhsu09@gmail.com>
2024-05-30 19:08:29 +0800

    add:storing data and validation

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Event;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,23 +14,40 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return Event::all();
     }
 
-    /**
+    /**  
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+
+
+        // 驗證請求數據
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+        ]);
+
+        // 合併驗證數據和其他數據
+        $data = array_merge($validatedData, [
+            'user_id' => 1,
+        ]);
+
+        // 使用合併後的數據創建事件
+        $event = Event::create($data);
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
